d87674596de41b45db1f3f844c46f3c4d226f0c3
MMartinezLopez <mauromarto07@gmail.com>
2023-03-08 23:25:42 -0500

    Creación portfolio

---
+++ src/app.js
@@ -0,0 +1,18 @@
+
+let current='home';
+window.addEventListener('scroll', (current)=>{
+    document.querySelectorAll("section").forEach(section=> {
+        if(window.scrollY >section.offsetTop-50){
+            current=section.id;
+        }
+    })
+
+    document.querySelectorAll(".navlink").forEach(link=>{
+    link.classList.remove("active");
+        if(link.href.includes(current)){
+
+            link.classList.add('active');
+        }
+    })
+})
+
