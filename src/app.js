44c9d0c9841d74783a48df3b085e241db7b86db8
MMartinezLopez <mauromarto07@gmail.com>
2023-05-28 23:18:20 -0500

    Bigger photo and correcting bugs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 
-window.addEventListener('scroll', (current)=>{
+window.addEventListener('scroll', ()=>{
     document.querySelectorAll("section").forEach(section=> {
         if(window.scrollY >section.offsetTop-50){
             current=section.id;
         }
     })
-
     document.querySelectorAll(".navlink").forEach(link=>{
     link.classList.remove("active");
         if(link.href.includes(current)){
